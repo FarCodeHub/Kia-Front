4999c414029607a6254c5a3804878a57e6ea45b8
Farhad Salehi <farcodehub@gmail.com>
2022-03-09 15:12:13 +0330

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/quotes */
 export const environment = {
     production: true,
-    apiUrl: "http://185.67.103.69:280/api",
-    singalUrl: "http://185.67.103.69:281/api",
-    imageUrl: "http://185.67.103.69:280",
+    apiUrl: "###",
+    singalUrl: "###",
+    imageUrl: "###",
 };
